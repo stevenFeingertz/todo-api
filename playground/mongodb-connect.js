f09ac8275c55e46fda41ce42f295013671fe93ce
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-14 18:06:53 -0500

    Add post todos route and refactor mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,3 +1,13 @@
+/*
+	MongoDB Notes
+	The mongodb db is not an application, it's a directory of executables. I placed it in my user (stevenFeingertz) folder. I then created a folder in the same directory called 'mongo-data'.
+
+	To start the db, I navigate to the bin directory of mongo db and type './mongod --dbpath ~/mongo-data'. I have to tell the db where the data is being stored.
+
+	I can also user robomongo application to get a gui interface for mongo db
+*/
+
+
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection url
---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {
+	mongoose: mongoose
+}
