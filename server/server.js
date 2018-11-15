7f90a10d261106804f18e7360c999a878beae2b0
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-14 20:29:45 -0500

    added find by id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
@@ -10,7 +11,7 @@ var app = express();
 app.use(bodyParser.json());
 
 
-
+// create
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
@@ -23,9 +24,33 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+// get todo list
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+// get single todo
+app.get('/todos/:id', (req, res) => {
+
+	//capture url param
+	var id = req.params.id;
+
+	// id valid?
+	if ( !ObjectID.isValid(id) ) {
+		return res.status(404).send(); // sending back empty body (404 status is not found)
+	}
+
+	// id is valid, try to find todo
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+		 return	res.status(404).send(); // sending back empty body (404 status is not found)
+		}
+
+		res.send({todo});
 	}, (e) => {
 		res.status(400).send(e);
 	});
