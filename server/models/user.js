0b1e8dd9ded03accff9fdb8f2ae59e4e350092d4
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-16 16:45:51 -0500

    added jwt tokens for creating users

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -33,6 +34,16 @@ var UserSchema = new mongoose.Schema({
 	}]
 });
 
+// OVERRIDE METHOD - This method is called automatically when the return from this schema converts to JSON.
+// I am intercepting it here to stop the whole document from being returned
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+// CUSTOM METHOD - we defined this, so it will only be called it we explicitly call it
 UserSchema.methods.generateAuthToken = function () {
 	var user = this;
 	var access = 'auth';
